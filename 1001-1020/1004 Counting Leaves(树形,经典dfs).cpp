cc2217d47b15a3af7fe9f8641c9aa6c9b4cd40cb
Mhhhaster <60060245+Mhhhaster@users.noreply.github.com>
2020-06-13 18:51:23 +0800

    update6.13

---
+++ 1001-1020/1004 Counting Leaves(树形,经典dfs).cpp
@@ -0,0 +1,40 @@
+﻿#include<vector>
+#include<iostream>
+using namespace std;
+vector<int>Par[100];
+vector<int>Output(100);
+int maxlevel = 0;
+void dfs(int id, int level)
+{
+	if (Par[id].size() == 0)
+		Output[level]++;
+	if (level > maxlevel)
+		maxlevel = level;
+	for (int i = 0; i < Par[id].size(); i++)
+		dfs(Par[id][i], level + 1);
+}
+int main()
+{
+	int N, M;
+	scanf("%d", &N);
+	if (!N)
+		return 0;
+	scanf("%d", &M);
+	int ID, childnum, childname;
+	for (int i = 0; i < M; i++)
+	{
+		scanf("%d%d", &ID, &childnum);
+		for (int i = 0; i < childnum; i++)
+		{
+			scanf("%d", &childname);
+			Par[ID].push_back(childname);
+		}
+	}
+	dfs(1, 0);
+	printf("%d", Output[0]);
+	for (int i = 1; i <= maxlevel; i++)
+		printf(" %d", Output[i]);
+	return 0;
+}
+/*经典树形结构的dfs
+和liuchuo的唯一不同就是她只在叶节点更新最大层数*/
---
+++ 1081-1100/1100 Mars Numbers(map,getline读取一行,包含所有范围).cpp
@@ -0,0 +1,68 @@
+﻿#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+string a[13] = { "tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug","sep", "oct", "nov", "dec" };
+string b[13] = { "####", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo","syy", "lok", "mer", "jou" };
+unordered_map<string, int>M1toE;//字符指向数字
+unordered_map<string, int>M2toE;
+void Judge()
+{
+    string input;
+    getline(cin,input);
+    if (input[0] - '0' >= 0 && input[0] - '0' <= 9)
+    {//0-169
+        int num;
+        sscanf(input.c_str(), "%d", &num);
+        if (num <= 12)//
+        {
+            cout << a[num % 13]<<endl;
+        }
+        else
+        {
+            cout << b[num / 13];
+            if (num % 13)
+                cout << " " << a[num % 13];
+            cout << endl;
+        }
+    }
+    else
+    {//3or4or7字符
+        int num;
+        if (input.length() == 4)
+            cout << "0" << endl;
+        else if (input.length() == 3)
+        {
+            num = M2toE[input] * 13 + M1toE[input];//找不到对应的就为0，不影响结果
+            cout << num << endl;
+        }
+        else
+        {
+            num = M2toE[input.substr(0, 3)] * 13 + M1toE[input.substr(4, 3)];
+            cout << num << endl;
+        }
+    }
+}
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    getchar();
+    for (int i = 0; i <= 12; i++)
+    {
+        M1toE[a[i]] = i;
+        M2toE[b[i]] = i;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        Judge();
+    }
+    return 0;
+}
+/*
+conclude:
+1.string 和int 类型的对应，采用map结构
+2.读取一行输入采用getline(cin,str)，注意getline之间是不用getchar来读取换行符的
+3.运用了sscanf和c_str()及substr函数。
+4.同样的，先在纸上计算好所有的取值范围和情况
+*/
