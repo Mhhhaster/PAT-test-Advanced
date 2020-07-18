906b707b56bb1354fddb2e4658cde95844cc3cf3
Mhhhaster <60060245+Mhhhaster@users.noreply.github.com>
2020-07-18 23:21:43 +0800

    update7.17

---
+++ 1141-1155/1150 Travelling Salesman Problem(图论).cpp
@@ -0,0 +1,65 @@
+﻿#include<iostream>
+#include<vector>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+int G[205][205];
+int N, M;
+const int uplimit = 99999999;
+int shortestdis = uplimit, index;
+void Judge(int t)
+{
+	bool visit[205] = { false };
+	int totaldis = 0;
+	int n;
+	scanf("%d", &n);
+	vector<int>path(n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &path[i]);
+	int start = path[0];
+	int end = path[n - 1];
+	for (int i = 0; i < n - 1; i++) {
+		totaldis += G[path[i]][path[i + 1]];
+		if (totaldis > uplimit) {
+			printf("Path %d: NA (Not a TS cycle)\n", t);
+			return ;
+		}
+	}
+	for (int i = 0; i < n; i++)
+		visit[path[i]] = true;
+	for (int i = 1; i <= N; i++) {
+		if (visit[i] == false) {
+			printf("Path %d: %d (Not a TS cycle)\n", t, totaldis);
+			return ;
+		}
+	}
+	if (start != end) {
+		printf("Path %d: %d (Not a TS cycle)\n", t, totaldis);
+		return;
+	}
+	if (totaldis < shortestdis) {
+		shortestdis = totaldis;
+		index = t;
+	}
+	if (n != N + 1) {
+		printf("Path %d: %d (TS cycle)\n", t, totaldis);
+		return ;
+	}
+	printf("Path %d: %d (TS simple cycle)\n", t, totaldis);
+}
+int main()
+{
+	scanf("%d%d", &N, &M);
+	fill(G[0], G[0] + 205 * 205, uplimit);
+	int V1, V2, Dis;
+	for (int i = 0; i < M; i++) {
+		scanf("%d%d%d", &V1, &V2, &Dis);
+		G[V1][V2] = G[V2][V1] = Dis;
+	}
+	int K;
+	scanf("%d", &K);
+	for (int t = 1; t <= K; t++)
+		Judge(t);
+	printf("Shortest Dist(%d) = %d", index, shortestdis);
+	return 0;
+}
---
+++ 1141-1155/1151 LCA in a Binary Tree(LCA是先序遍历中最先满足在中序遍历中位于中间的结点).cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+int main()
+{
+    int N, M;
+    scanf("%d%d", &N, &M);
+    unordered_map<int, int>inpos;
+    vector<int>in(M + 1);
+    vector<int>pre(M + 1);
+    for (int i = 1; i <= M; i++) {
+        scanf("%d", &in[i]);
+        inpos[in[i]] = i;
+    }
+    for (int i = 1; i <= M; i++)
+        scanf("%d", &pre[i]);
+    int u, v;
+    for (int i = 0; i < N; i++) {
+        scanf("%d%d", &u, &v);
+        int posu = inpos[u];
+        int posv = inpos[v];
+        if (posu == 0 || posv == 0) {//说明有一个找不到
+            if (posv == posu)
+                printf("ERROR: %d and %d are not found.\n", u, v);
+            else
+                printf("ERROR: %d is not found.\n", posu == 0 ? u : v);
+        }
+        else {
+            int a = -1;
+            for (int i = 1; i <= M; i++) {
+                a = inpos[pre[i]];//遍历一遍pre数组
+                if ((a <= posv && a >= posu) || (a <= posu && a >= posv))
+                    break;
+            }
+            if (a == posu || a == posv)
+                printf("%d is an ancestor of %d.\n", in[a], a == posv ? u : v);
+            else
+                printf("LCA of %d and %d is %d.\n", u, v, in[a]);
+        }
+    }
+}
